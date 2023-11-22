dd1177d0809f3fe1ba0a73f4cabaa79f3651827d
Donovan Castrillon <dcastrillf@eafit.edu.co>
2023-11-22 00:30:07 -0500

    el mani el gei

--- pagina/recommendations/algo.py
+++ pagina/recommendations/algo.py
@@ -4,24 +4,27 @@ def do_recomendation(userLikedEvents: dict, allEvents: dict, isAuth: bool) -> di
     categoryCounter = {}
 
     if isAuth is True:
-        # Get tha category of the events that user liked and count how many of each category are
-        for i in range(len(userLikedEvents)):
-            for event in allEvents:
-                if event['id'] == userLikedEvents[i]['id']:
-                    category = event['category']
-                    categoryCounter[category] = categoryCounter.get(category, 0) + 1
-
-        # Take all events with the category that has more likes, if there is a tie it returns all categories involved
-        maxQuantity = max(categoryCounter.values())
-        maxAmmountCategories = [categoria for categoria, cantidad in categoryCounter.items() if cantidad == maxQuantity]
-        specificEventsMax = [evento for evento in allEvents if evento['category'] in maxAmmountCategories]
-
-        # return the results
-
-        if len(specificEventsMax) >= 3:
-            return sample(specificEventsMax, 3)
+        if len(userLikedEvents) == 0:
+            return {}
         else:
-            return specificEventsMax
+            # Get tha category of the events that user liked and count how many of each category are
+            for i in range(len(userLikedEvents)):
+                for event in allEvents:
+                    if event['id'] == userLikedEvents[i]['id']:
+                        category = event['category']
+                        categoryCounter[category] = categoryCounter.get(category, 0) + 1
+
+            # Take all events with the category that has more likes, if there is a tie it returns all categories involved
+            maxQuantity = max(categoryCounter.values())
+            maxAmmountCategories = [categoria for categoria, cantidad in categoryCounter.items() if cantidad == maxQuantity]
+            specificEventsMax = [evento for evento in allEvents if evento['category'] in maxAmmountCategories]
+
+            # return the results
+
+            if len(specificEventsMax) >= 3:
+                return sample(specificEventsMax, 3)
+            else:
+                return specificEventsMax
     elif isAuth is False:
         if len(allEvents) >= 3:
             return sample(allEvents, 3)
