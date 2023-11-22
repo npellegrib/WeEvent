637f9fd7d76cff612fdd974d61212835acbb189a
Donovan Castrillon <dcastrillf@eafit.edu.co>
2023-11-22 00:28:02 -0500

    more bugs

--- pagina/recommendations/algo.py
+++ pagina/recommendations/algo.py
@@ -4,27 +4,24 @@ def do_recomendation(userLikedEvents: dict, allEvents: dict, isAuth: bool) -> di
     categoryCounter = {}
 
     if isAuth is True:
-        if len(userLikedEvents) == 0:
-            return {'msg':'Empieza a interactuar con la pagina para recomendarte eventos'}
+        # Get tha category of the events that user liked and count how many of each category are
+        for i in range(len(userLikedEvents)):
+            for event in allEvents:
+                if event['id'] == userLikedEvents[i]['id']:
+                    category = event['category']
+                    categoryCounter[category] = categoryCounter.get(category, 0) + 1
+
+        # Take all events with the category that has more likes, if there is a tie it returns all categories involved
+        maxQuantity = max(categoryCounter.values())
+        maxAmmountCategories = [categoria for categoria, cantidad in categoryCounter.items() if cantidad == maxQuantity]
+        specificEventsMax = [evento for evento in allEvents if evento['category'] in maxAmmountCategories]
+
+        # return the results
+
+        if len(specificEventsMax) >= 3:
+            return sample(specificEventsMax, 3)
         else:
-            # Get tha category of the events that user liked and count how many of each category are
-            for i in range(len(userLikedEvents)):
-                for event in allEvents:
-                    if event['id'] == userLikedEvents[i]['id']:
-                        category = event['category']
-                        categoryCounter[category] = categoryCounter.get(category, 0) + 1
-
-            # Take all events with the category that has more likes, if there is a tie it returns all categories involved
-            maxQuantity = max(categoryCounter.values())
-            maxAmmountCategories = [categoria for categoria, cantidad in categoryCounter.items() if cantidad == maxQuantity]
-            specificEventsMax = [evento for evento in allEvents if evento['category'] in maxAmmountCategories]
-
-            # return the results
-
-            if len(specificEventsMax) >= 3:
-                return sample(specificEventsMax, 3)
-            else:
-                return specificEventsMax
+            return specificEventsMax
     elif isAuth is False:
         if len(allEvents) >= 3:
             return sample(allEvents, 3)
